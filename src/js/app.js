ce66d55115b90876b96532a0542a95f1692cca6d
macotok <monochromer@r8.ucom.ne.jp>
2018-06-24 12:44:01 +0900

    markdownのstyleを選択式に変更

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,39 @@ import { h, app } from 'hyperapp';
 import marked from 'marked';
 import '../scss/style.scss';
 
+const previewStyles = ['github', 'air'];
 const state = {
   preview: '',
+  previewStyle: previewStyles[0],
 };
 
 const actions = {
   setInput: input => state => ({
     preview: marked(input),
   }),
+  changeCss: input => state => ({
+    previewStyle: input,
+  }),
 };
 
 const view = (state, actions) => (
   <div>
+    <header id="header">
+      <h1>Hypertapp Markdown Editor</h1>
+      <p id="selectWrap">
+        <label>
+          CSS type :
+          <select
+            id="selectCss"
+            onchange={e => actions.changeCss(e.target.value)}
+          >
+            {previewStyles.map(previewStyle => (
+              <option value={previewStyle}>{previewStyle}</option>
+            ))}
+          </select>
+        </label>
+      </p>
+    </header>
     <article id="main">
       <section id="inputMarkdown">
         <p id="editorWrap">
@@ -25,7 +46,7 @@ const view = (state, actions) => (
         </p>
       </section>
       <section id="previewHtml">
-        <div id="preview" innerHTML={state.preview}></div>
+        <div id="preview" className={state.previewStyle} innerHTML={state.preview}></div>
       </section>
     </article>
   </div>
